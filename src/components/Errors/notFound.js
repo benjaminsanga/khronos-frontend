608f4e1f2b79905b9794a627d404d45acd0b3392
benjaminsanga <benjotaiba@gmail.com>
2024-08-24 22:07:56 +0100

    fixed login, deposit mobile view

--- src/components/Errors/notFound.js
+++ src/components/Errors/notFound.js
@@ -4,13 +4,12 @@ import Emergency from '../../assets/icons/happy.svg';
 const NotFound = () => {
     return (
         <>
-            <div className="row" style={{maxWidth: '100%', padding: '5% 0 10px'}}>
+            <div className="row" style={{height: '70vh', padding: '10px'}}>
                 <div className="col-md-3"></div>
                 <div className="col-md-6">
                     <div className="d-flex flex-column align-items-center justify-content-between success">
-                        <img src={Emergency} alt="Email" style={{width: '10%', height: 'relative'}} />
-                        <h3>Off the grid.</h3>
-                        <span style={{fontSize: '12rem'}}>404</span>
+                        <img src={Emergency} alt="Email" style={{width: '75px', height: 'relative'}} />
+                        <span style={{fontSize: '6rem'}}>404</span>
                         <h5>Page not found</h5>
                         <Link to='/'>
                             <button className="btn btn-lg btn-primary fw-lighter mt-2">Back to Home</button>
--- src/components/Errors/unAuthorized.js
+++ src/components/Errors/unAuthorized.js
@@ -4,13 +4,13 @@ import Emergency from '../../assets/icons/happy.svg';
 const LoginFirst = () => {
     return (
         <>
-            <div className="row my-5 w-100">
+            <div className="row my-5 p-3 w-100" style={{height: '70vh'}}>
                 <div className="col-md-3"></div>
                 <div className="col-md-6">
                     <div className="d-flex flex-column align-items-center justify-content-between success">
-                        <img src={Emergency} alt="Email" style={{width: '10%', height: 'relative'}} />
+                        <img src={Emergency} alt="Email" style={{width: '125px', height: 'relative'}} />
                         <h3 className='mb-5'>Off The Grid</h3>
-                        <h5 className='text-center mb-5'>To enjoy all the features, please create an account. <br/>Once your account is set up, you can <a href='/login' className='text-secondary text-decoration-none'><i>Login</i></a> and start exploring.</h5>
+                        <h5 className='text-center mb-5'>Register to use this feature.</h5>
                         <Link to='/create-account'>
                             <button className="btn btn-lg btn-primary fw-lighter mt-2">Register</button>
                         </Link>
