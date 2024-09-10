fa23b00022562560bdb9e8fd2ff0e9eb1b2652f6
benjaminsanga <benjotaiba@gmail.com>
2024-09-10 18:12:30 +0100

    updated policy, terms, faq layout

--- src/components/privacy/privacyPolicyPage.js
+++ src/components/privacy/privacyPolicyPage.js
@@ -2,15 +2,15 @@
 
 const PrivacyPolicyPage = () => {
     return (
-        <div id="privacy-policy" className="container">
+        <div id="privacy-policy" className="container-fluid">
             <section className="row">
-                <div className="col-md-6">
+                <div className="col-md-6 p-0">
                     <h1>Privacy Policy</h1>
                     <p>We care about your data.</p>
                 </div>
             </section>
-            <section className="row">
-                <div>
+            <section className="row p-0">
+                <div className="p-0">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
