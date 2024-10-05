6e672d67cc40768e8cc672b677f611495a42552b
benjaminsanga <benjotaiba@gmail.com>
2024-10-05 12:57:07 +0100

    fix: updated terms and conditions

--- src/components/privacy/privacyPolicyPage.js
+++ src/components/privacy/privacyPolicyPage.js
@@ -11,11 +11,46 @@ const PrivacyPolicyPage = () => {
             </section>
             <section className="row p-0">
                 <div className="p-0">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                Effective Date: [Insert Effective Date]
 
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                Welcome to Khronos! At Khronos, we value your privacy and are committed to safeguarding your personal information. This Privacy Policy outlines how we collect, use, and protect your data when you use our platform. By accessing or using our application, you consent to the terms described in this policy.
 
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                1. Information We Collect
+                We collect various types of information to improve your experience with Khronos, including:
+
+                Personal Information: Information you provide when creating an account, such as your name, email address, and other identifying details.
+                Transaction Information: Details of transactions made through Khronos, including the amount, date, and associated projects.
+                Device Information: Information about the device and network used to access Khronos, such as IP address and browser type.
+                Usage Data: Information about your interactions with our platform, such as pages visited, time spent on the site, and features used.
+                2. How We Use Your Information
+                We use your information to:
+
+                Provide and maintain the Khronos service.
+                Facilitate transactions and connect communities with projects.
+                Enhance security and prevent fraudulent activities.
+                Communicate updates, promotions, and important notices.
+                Analyze usage trends to improve our services.
+                3. Sharing Your Information
+                We do not sell, rent, or trade your personal information. However, we may share your data with:
+
+                Service Providers: Third-party service providers who assist us in delivering our services.
+                Legal Compliance: If required by law or in response to valid legal requests.
+                Partners: With your consent, we may share information with partners for collaborative community projects.
+                4. Data Security
+                We prioritize the security of your data. We employ advanced encryption techniques and maintain robust safeguards to protect against unauthorized access, loss, or misuse of your information.
+
+                5. Your Rights
+                You have the right to:
+
+                Access and update your personal information.
+                Request the deletion of your data.
+                Opt-out of marketing communications.
+                Withdraw consent at any time for data processing.
+                6. Changes to the Privacy Policy
+                We may update this Privacy Policy periodically. Any changes will be communicated through our platform or via email. Continued use of the application after such updates constitutes your acceptance of the revised policy.
+
+                7. Contact Us
+                If you have any questions or concerns about this Privacy Policy, please contact us at [Insert Contact Email].
                 </div>
             </section>
         </div>
--- src/components/terms/termsPage.js
+++ src/components/terms/termsPage.js
@@ -11,11 +11,89 @@ const TermsPage = () => {
             </section>
             <section className="row">
                 <div className="p-0">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>Effective Date: 5<sup>th</sup> October, 2024</p>
 
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>
+                        Welcome to Khronos! These Terms and Conditions ("Terms") govern your use of the Khronos application and its services. By accessing or using our platform, you agree to comply with these Terms.
+                    </p>
+                    <ol>
+                        <li>
+                            <h5>Acceptance of Terms</h5>
+                            <p>
+                                By using Khronos, you acknowledge that you have read, understood, and agree to these Terms and our Privacy Policy. If you do not agree with any part of the Terms, please discontinue using our services immediately.
+                            </p>
+                        </li>
+                        <li>
+                            <h5>Account Creation and Usage</h5>
+                            <ul>
+                                <li>
+                                    You must create an account to use Khronos. By creating an account, you represent that the information provided is accurate, complete, and up-to-date.
+                                </li>
+                                <li>
+                                    You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account.
+                                </li>
+                                <li>
+                                    You may not use Khronos for any illegal or unauthorized purposes.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h5>Services Provided</h5>
+                            <p>
+                                Khronos provides a digital wallet and financial ecosystem designed to empower communities and facilitate transactions. Our services include:
+                            </p>
+                            <ul>
+                                <li>Digital wallet creation and management.</li>
+                                <li>Seamless financial contributions to community projects.</li>
+                                <li>Transparency and tracking of project funding and completion.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h5>User Obligations</h5>
+                            <p>As a user of Khronos, you agree to:</p>
+                            <ul>
+                                <li>Use the platform in compliance with all applicable laws and regulations.</li>
+                                <li>Respect the rights and privacy of other users.</li>
+                                <li>Refrain from engaging in activities that may harm or disrupt the platform or other users.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h5>Intellectual Property</h5>
+                            <p>
+                                All content and materials available on Khronos, including text, graphics, logos, and software, are the property of Khronos or its licensors and are protected by intellectual property laws. You may not use, copy, or distribute any content without our prior written permission.
+                            </p>
+                        </li>
+                        <li>
+                            <h5>Limitation of Liability</h5>
+                            <p>
+                                Khronos is not liable for any indirect, incidental, or consequential damages arising from your use of the platform. Our liability for any claim related to your use of the services is limited to the amount paid, if any, to access the platform.
+                            </p>
+                        </li>
+                        <li>
+                            <h5>Termination</h5>
+                            <p>
+                                We reserve the right to suspend or terminate your account if you violate these Terms or engage in activities that may harm Khronos or its users.
+                            </p>
+                        </li>
+                        <li>
+                            <h5>Changes to the Terms</h5>
+                            <p>
+                                We may update these Terms from time to time. Changes will be communicated through the platform or via email. Your continued use of Khronos after such changes indicates your acceptance of the revised Terms.
+                            </p>
+                        </li>
+                        <li>
+                            <h5>Governing Law</h5>
+                            <p>
+                                These Terms are governed by and construed in accordance with the laws of The Federal Republic of Nigeria. Any disputes arising under or in connection with these Terms shall be resolved exclusively in the courts of The Federal Republic of Nigeria.
+                            </p>
+                        </li>
+                        <li>
+                            <h5>Contact Us</h5>
+                            <p>
+                                For any questions or concerns about these Terms, please contact us at sales@netaccessnigeria.com.
+                            </p>
+                        </li>
+                    </ol>
                 </div>
             </section>
         </div>
